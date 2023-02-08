f50a43802cd8f73d1311ac9fa8e9be7ad9fe0172
Imac 4 <imac4@Imacs-iMac.local>
2023-02-08 06:40:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+// useEffect load the API as soon as the component loads
+
+// call API
+// 28c69be5
+
+// 1 - static variable
+// const movie1 = {};
+
+const API_URL = "http://www.omdbapi.com?apikey=28c69be5";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Halloween");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pp.js
@@ -0,0 +1,34 @@
+// import { useState, useEffect } from "react";
+// import "./App.css";
+
+// const App = () => {
+//   const name = "Janne";
+//   const isNameShowing = true;
+
+//   const [counter, setCounter] = useState(0);
+//   useEffect(() => {});
+
+//   return (
+//     <div className="App">
+//       <h1>Hello, {isNameShowing ? name : "someone"}</h1>
+//       {name ? (
+//         <>{name}</>
+//       ) : (
+//         <>
+//           <h1>test</h1> <h2>there is no name</h2>
+//         </>
+//       )}
+//       <div>
+//         <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+//           -
+//         </button>
+//         <h1>{counter}</h1>
+//         <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+//           +
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
